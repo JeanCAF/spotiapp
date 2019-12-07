b8621190e2b6c1e76e0b69f8bcb77d45335804a6
juanogit <jcarayaf64@gmail.com>
2019-12-07 13:59:39 +0100

    nueva actualizacion clase 97

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,20 +15,22 @@ export class SpotifyService {
    getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCIBKhaFudMBCDsZL9Mhzv0cG9PbvrBw3U7zIFJMYOJJ-ttzxuX3DmDbN2P68RquD3jOCzegWdGszPchZQ'
+      'Authorization': 'Bearer BQBqcPYKnGzM46-7VpQFET4OHptiGLTmFn0DdCPm9w2RNJd2gBD8Bvm_CrirWI6X2ZbHy-Vp3JOde3AVaq0'
      });
 
-    return this.http.get( 'https://api.spotify.com/v1/browse/new-releases', { headers } );
+    return this.http.get( 'https://api.spotify.com/v1/browse/new-releases', { headers } )
+              .pipe( map( data => data['albums'].items));
 
    }
 
    getArtista(termino: string) {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCIBKhaFudMBCDsZL9Mhzv0cG9PbvrBw3U7zIFJMYOJJ-ttzxuX3DmDbN2P68RquD3jOCzegWdGszPchZQ'
+      'Authorization': 'Bearer BQBqcPYKnGzM46-7VpQFET4OHptiGLTmFn0DdCPm9w2RNJd2gBD8Bvm_CrirWI6X2ZbHy-Vp3JOde3AVaq0'
      });
 
-    return this.http.get( `https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers } );
+    return this.http.get( `https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers } )
+              .pipe( map( data => data['artists'].items));
 
     }
 }
